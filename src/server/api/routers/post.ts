c1527ce964c75a7379bb82e76a3786c9d5608b45
belkhiria.hatem <belkhiria.hatem@satoripop.com>
2023-06-15 12:08:00 +0100

    profile page, ssg

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,10 +1,10 @@
 import { clerkClient } from "@clerk/nextjs";
-import type { User } from "@clerk/nextjs/dist/types/server";
 import { z } from "zod";
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 import { TRPCError } from "@trpc/server";
+import { filterUser4Client } from "~/server/helpers/filterUser4Client";
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
@@ -13,14 +13,6 @@ const ratelimit = new Ratelimit({
   analytics: true,
   prefix: "@upstash/ratelimit",
 });
-
-const filterUser4Client = (user: User) => {
-  return {
-    id: user.id, 
-    username: user.username, 
-    profileImageUrl: user.profileImageUrl
-  }
-}
 
 export const postRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
